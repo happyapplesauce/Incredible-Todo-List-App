ec652cf8962883109ceb6d25b3e4d182fb4941f8
Kashish Gour <141449101+happyapplesauce@users.noreply.github.com>
2024-05-27 13:00:21 -0600

    Add files via upload

---
+++ app/(tabs)/Custom_text.jsx
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native'
+import React,{useState} from 'react'
+
+const Custom_text = () => {
+
+  const[name,setName]=useState("Bob");
+  return (
+    <View>
+      <Text style={{fontSize:45,textAlign:"center"}}Counter is: {counter</Text>
+      <Button title="Increment" onPress={()=>setCount( count+1)}></Button>
+      <Text style={{Updatedtext}}
+    </View>
+  )
+}
+
+export default Custom_text
---
+++ app/(tabs)/newtab.tsx
@@ -0,0 +1,15 @@
+import { Button, StyleSheet, Text, View, Image } from 'react-native'
+import React from 'react'
+
+const newtab = () => {
+  return (
+    <View>
+      <Text>newtab</Text>
+      <Button title = "Press Me" onPress={()=>alert('Button pressed')}></Button>
+      <Image source ={{}}
+      
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
